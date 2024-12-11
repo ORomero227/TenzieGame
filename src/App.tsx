475080b0d67584b3b9bdf6239d4d4e10ca5cba98
Oscar Romero <oscalito992@gmail.com>
2024-12-11 10:09:58 -0400

    Title and instructions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-semibold underline">Tenzie Game</h1>;
+  return (
+    <main className="p-4 md:p-0 max-w-full">
+      <section className="md:max-w-xl h-[400px] mx-auto my-36 p-10 rounded-xl bg-[#f5f5f5]">
+        <h1 className="text-[#2b283a] font-karla tracking-tighter text-center font-semibold text-3xl">
+          Tenzies
+        </h1>
+        <p className="mt-3 mx-auto font-inter font-normal text-base text-center text-[#4A4E74]">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
